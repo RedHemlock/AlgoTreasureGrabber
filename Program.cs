f7eaca57493ee7629372d91fc3c4fc5c9ab244fa
Luke Kedrowski <110366021+RedHemlock@users.noreply.github.com>
2025-10-06 15:52:39 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using Treasure_Grabber;
+
+//merge implementation
+void Merge(Treasure[] arr,int left,int right,int middle)
+{
+    Treasure[] low = new Treasure[middle - left + 1];
+    Treasure[] high = new Treasure[right - middle];
+    for(int a = 0;a < low.Length;a++)
+    {
+        low[a] = arr[a + left];
+    }
+    for(int a = 0; a < high.Length; a++)
+    {
+        high[a] = arr[a + middle + 1];
+    }
+    int i = 0;
+    int j = 0;
+    int k = left;
+    while(i < low.Length && j < high.Length)
+    {
+        if (low[i].weightedValue >= high[j].weightedValue)
+        {
+            arr[k] = low[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            arr[k] = high[j];
+            k++;
+            j++;
+        }
+    }
+    while (i < low.Length)
+    {
+        arr[k] = low[i];
+        k++;
+        i++;
+    }
+    while(j < high.Length)
+    {
+        arr[k] = high[j];
+        k++;
+        j++;
+    }
+}
+//mergesort implementation
+ void mergeSort(Treasure[] arr,int left,int right) { 
+    int mid = left +(right-left)/2;
+    if (left < right) {
+        mergeSort(arr,left,mid);
+        mergeSort(arr, mid+1,right);
+        Merge(arr,left, right,mid);
+    }
+}
+
+
+
+Bag bag = new Bag(4000);
+//get file location
+Console.WriteLine("Insert Path to File: ");
+string fileName = Console.ReadLine().Trim();
+Stopwatch time = Stopwatch.StartNew();
+//opens file
+
+StreamReader reader = new StreamReader(fileName);
+string line;
+int count = 0;
+//counts number of lines in file
+while ((line = reader.ReadLine()) != null)
+{
+    count++;
+}
+reader.Close();
+//creates a list that is the size of the file
+Treasure[] cave = new Treasure[count];
+count = 0;
+//reopens file
+reader = new StreamReader(fileName);
+//adds each treasure to cave(list)
+while ((line = reader.ReadLine()) != null)
+{
+    string name = line.Substring(0,line.IndexOf(','));
+    line = line.Substring(line.IndexOf(',') + 1);
+    int value = Convert.ToInt32(line.Substring(0,line.IndexOf(',')));
+    line = line.Substring(line.IndexOf(',') + 1);
+    int weight = Convert.ToInt32(line);
+    cave[count] = (new Treasure(name, value, weight));
+    count++;
+}
+//sort the cave by value per weight
+mergeSort(cave, 0, cave.Length-1);
+//initaiting tracking values
+int currweight = 0;
+int totalValue = 0;
+foreach (Treasure t in cave)
+{
+    //assuemes cave is sorted puts each item that can fit into the bag into the bag and keeps track of value and weight
+    if (currweight + t.weight < bag.maxWeight)
+    {
+        currweight += t.weight;
+        bag.container.Add(t);
+        totalValue += t.value;
+    }
+}
+
+Console.WriteLine("Total Bag Value: " + totalValue);
+Console.WriteLine("Total Bag Weight: " + currweight);
+Console.WriteLine("Time Taken: " + time.ToString());
---
+++ Treasureclasses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Treasure_Grabber
+{
+    public struct Treasure
+    {
+        public string name;
+        public int value;
+        public int weight;
+        public double weightedValue;
+
+        public Treasure(string name, int value, int weight)
+        {
+            this.name = name;
+            this.value = value;
+            this.weight = weight;
+            this.weightedValue = (double)value/weight;
+        }
+    }
+    public struct Bag
+    {
+        public int maxWeight;
+        public ArrayList container;
+
+        public Bag(int weight)
+        {
+            maxWeight = weight;
+            container = new ArrayList();
+        }
+    }
+}
